ca20114644006c5cb21e24b61701c0ab7f9a9374
Wisdom Arerosuoghene <wisdom.arerosuoghene@gmail.com>
2019-04-27 15:42:17 +0100

    Initial Commit

---
+++ AutoResizeFont/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoResizeFont
+//
+//  Created by Wisdom Arerosuoghene on 27/04/2019.
+//  Copyright © 2019 Wisdom Arerosuoghene. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
