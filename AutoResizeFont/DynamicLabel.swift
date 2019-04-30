e14c9a40b663e11e210eb48b3d79aa4495fcf6a0
Mark Boleigha <donhoenix@gmail.com>
2019-04-30 11:42:40 +0100

    override font defaults
    Modified custom Init and made text resize based on height of the component and device type. font is overridden and set based on detected device type. results in bigger fonts for bigger screens and initial font setting for smaller screens.

--- AutoResizeFont/DynamicLabel.swift
+++ AutoResizeFont/DynamicLabel.swift
@@ -10,38 +10,60 @@ import UIKit
 
 @IBDesignable
 final class DynamicLabel: UILabel {
+    
+    //allow font to accept size from interface builder and scale up from there.
+    //this is to let us use values from the design specs
     @IBInspectable
-    var fontSizePts: CGFloat {
-        didSet {
-            calculateFontSize()
+    var pointSize = UIFont() {
+        didSet{
+            self.font = pointSize
+        }
+    }
+    
+    //override font for view
+    override var font : UIFont?{
+        didSet{
+            setNeedsLayout()
             setNeedsDisplay()
         }
     }
     
+    //deduced by multiplying the height of the Label by a constant
+    var relativeHeight : CGFloat!
+    
+    //relative constant to multiply our initial element height.
+    //this lets us double the size on bigger devices
+    let relativeFontConstant:CGFloat = 0.1
+    
     override init(frame: CGRect) {
-        fontSizePts = 14
-        super.init(frame: frame);
+        super.init(frame: frame)
+        relativeHeight = CGFloat(frame.height * relativeFontConstant)
+        calculateFontSize()
     }
     
     required init?(coder aDecoder: NSCoder) {
-        fontSizePts = 14
-        super.init(coder: aDecoder);
+        super.init(coder: aDecoder)
+        relativeHeight = CGFloat(frame.height * relativeFontConstant)
+        calculateFontSize()
     }
     
-    // todo calculate fonts for iPad and iPod
+    // todo calculate fonts for iPad, iPhone and iPod
     func calculateFontSize() {
         switch UIDevice().type {
         case .iPhoneSE, .iPhone5, .iPhone5S, .iPhone5C:
-            self.font = self.font.withSize(self.fontSizePts)
+            pointSize = self.font!.withSize(self.font!.pointSize)
         case .iPhone6, .iPhone6S, .iPhone7, .iPhone8:
-            self.font = self.font.withSize(self.fontSizePts * 2)
+            self.font = self.font!.withSize(self.font!.pointSize * (relativeHeight / 3))
         case .iPhone6plus, .iPhone6Splus, .iPhone7plus, .iPhone8plus:
-            self.font = self.font.withSize(self.fontSizePts * 2)
+            pointSize = self.font!.withSize(self.font!.pointSize * (relativeHeight / 3) )
         case .iPhoneX, .iPhoneXR, .iPhoneXS, .iPhoneXSMax:
-            self.font = self.font.withSize(self.fontSizePts * 3)
+            pointSize = self.font!.withSize(self.font!.pointSize * (relativeHeight / 2) )
+        case .iPad2, .iPad3, .iPad4, .iPad5, .iPad6, .iPadAir, .iPadMini, .iPadPro2_12_9:
+            pointSize = self.font!.withSize(self.font!.pointSize * relativeHeight)
+        case .iPod1, .iPod2, .iPod3, .iPod4, .iPod5:
+            pointSize = self.font!.withSize(self.font!.pointSize * (relativeHeight / 4 ) )
         default:
-            self.font = self.font.withSize(self.fontSizePts * 4)
+            pointSize = self.font!.withSize(self.font!.pointSize * relativeHeight)
         }
-        print(self.font.pointSize)
     }
 }
--- AutoResizeFont/ViewController.swift
+++ AutoResizeFont/ViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var label1 : DynamicLabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
